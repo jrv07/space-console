1d9e376ed711ae7931328a76646ebc1ccea64578
Vaidya Jugal Rasendra <JugalRasendra.Vaidya@arrk-engineering.com>
2025-04-13 13:17:41 +0200

    update search bar asthetics

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -3,11 +3,12 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { setLoading, setResults, setError } from '../../redux/store';
-import { API_BASE_URL } from '../../config'; // Import config
+import { API_BASE_URL } from '../../config';
 import './SearchBar.css';
 
 const SearchBar = () => {
   const [query, setQuery] = useState('');
+  const [isFocused, setIsFocused] = useState(false); // Track focus
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -17,19 +18,21 @@ const SearchBar = () => {
     try {
       const response = await axios.post(`${API_BASE_URL}/api/query`, { query });
       dispatch(setResults(response.data));
-      navigate('/search'); // Navigate to search page after query
+      navigate('/search');
     } catch (err) {
       dispatch(setError('Failed to fetch data'));
     }
   };
 
   return (
-    <div className="search-bar">
+    <div className={`search-bar ${isFocused ? 'focused' : ''}`}>
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         onKeyPress={(e) => e.key === 'Enter' && handleSearch()}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
         placeholder="Ask a question (e.g., 'sales, products, users')"
       />
     </div>
